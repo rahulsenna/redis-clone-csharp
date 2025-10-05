917c62575211e1e7ead9d295357cb521d308d5c2
Rahul Senna <rahulvsenna@gmail.com>
2025-10-05 19:43:07 +0530

    Set/Get

--- src/Server.cs
+++ src/Server.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -7,7 +8,7 @@ const int DEFAULT_BUFFER_SIZE = 1024;
 
 TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
-
+ConcurrentDictionary<string, RedisValue> _db = [];
 while (true)
 {
   Socket socket = await server.AcceptSocketAsync();
@@ -26,14 +27,44 @@ async Task HandleClient(Socket socket)
     if (bytesRead <= 0) break;
 
     string str = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-    var commands = str.Split("\r\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Where((_, idx) => idx % 2 == 0).ToArray();
-    string command = commands[1];
+    var query = str.Split("\r\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Where((_, idx) => idx % 2 == 0).ToArray();
+    string command = query[1];
 
     if (command == "PING")
       await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"));
 
     else if (command == "ECHO")
-      await socket.SendAsync(Encoding.UTF8.GetBytes($"+{commands[2]}\r\n"));
-  }
+      await socket.SendAsync(Encoding.UTF8.GetBytes($"+{query[2]}\r\n"));
 
+    else if (command == "SET")
+    {
+      long? ExpireAt = null;
+      if (query.Length > 5 && query[4].Equals("px", StringComparison.OrdinalIgnoreCase))
+        ExpireAt = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() + Convert.ToInt64(query[5]);
+
+      string key = query[2];
+      _db[key] = new RedisValue(RedisType.String, query[3], ExpireAt);
+      await socket.SendAsync(Encoding.UTF8.GetBytes("+OK\r\n"));
+    }
+    else if (command == "GET")
+    {
+      string key = query[2];
+
+      if (!_db.TryGetValue(key, out var value) || value.IsExpired())
+        await socket.SendAsync(Encoding.UTF8.GetBytes("$-1\r\n"));
+      else
+        await socket.SendAsync(Encoding.UTF8.GetBytes($"+{value.Data as string}\r\n"));
+    }
+  }
 }
+
+public enum RedisType { None, String }
+
+public sealed record RedisValue(
+  RedisType Type,
+  object? Data,
+  long? ExpireAtMs
+)
+{
+  public bool IsExpired() => ExpireAtMs is long && ExpireAtMs <= DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+};
