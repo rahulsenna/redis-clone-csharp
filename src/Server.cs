b5884f441cc88d493fef336160b5b632fef78b3a
Rahul Senna <rahulvsenna@gmail.com>
2025-10-05 05:39:30 +0530

    PING

--- src/Server.cs
+++ src/Server.cs
@@ -1,8 +1,36 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Logs from your program will appear here!");
+
+const int DEFAULT_BUFFER_SIZE = 1024;
 
 TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+while (true)
+{
+  Socket socket = await server.AcceptSocketAsync();
+  // _ = HandleClient(socket);
+  _ = Task.Run(() => HandleClient(socket));
+
+}
+
+async Task HandleClient(Socket socket)
+{
+  using var _ = socket; // Auto dispose at end of function 
+  byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+  while (true)
+  {
+    int bytesRead = await socket.ReceiveAsync(buffer);
+    if (bytesRead <= 0) break;
+
+    string str = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    var commands = str.Split("\r\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Where((_, idx) => idx % 2 == 0).ToArray();
+    string command = commands[1];
+
+    if (command == "PING")
+      await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"));
+  }
+
+}
